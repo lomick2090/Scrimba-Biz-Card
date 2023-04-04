313574b1fe4b21687c712c15e8f60b3e2a155830
minkman <lomick2090@gmail.com>
2023-04-04 11:11:16 -0500

    Add email icon

--- biz-card/src/components/Heading.jsx
+++ biz-card/src/components/Heading.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Mail from '../Mail.png'
 
 export default function Heading() {
     return (
@@ -6,7 +7,7 @@ export default function Heading() {
             <h1 className="name">Jesse Stahl</h1>
             <h4 className="title">Front-End Developer</h4>
             <h5 className="website"><a href="https://gomeditate.net">GoMeditate.net</a></h5>
-            <a href="mailto:lomick2090@gmail.com"><button className ='email'>Email</button></a>
+            <a href="mailto:lomick2090@gmail.com" className='emaillink'><button className ='email'><span><img src={Mail}/></span>Email</button></a>
         </div>
     )
 }
